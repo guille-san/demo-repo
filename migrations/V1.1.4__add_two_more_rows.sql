45485b4e7d66581bed2e3ab0745db070c0ca18f8
Guillermo San Arranz <guillermo.san@bain.com>
2024-03-27 08:36:06 -0500

    Adding Saurabh and Waldek to hello_world

---
+++ migrations/V1.1.4__add_two_more_rows.sql
@@ -0,0 +1,23 @@
+USE SCHEMA DEMO;
+
+INSERT INTO HELLO_WORLD (
+    FIRST_NAME,
+    LAST_NAME,
+    AGE
+)
+VALUES(
+    'Waldemar',
+    'GZ',
+    45
+);
+
+INSERT INTO HELLO_WORLD (
+    FIRST_NAME,
+    LAST_NAME,
+    AGE
+)
+VALUES(
+    'Saurabh',
+    'Gupta',
+    30
+);
